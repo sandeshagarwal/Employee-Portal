bc031d072903a5b0e8da4742fcb84cc342ccfac0
unknown <sandeshagarwal725@gmail.com>
2020-11-23 09:31:12 +0530

    crud operation react.fullstack

---
+++ CrudFullstackWithReact/README.md
@@ -0,0 +1,5 @@
+# CRUD Operation
+Here, I have created a Employee Portal where we can add,delete,get,update employees in the database with the help of react.
+This is a fullstack application where we can do crud from the frontend.
+The Tech stack used is Spring Boot, JPA, Hibernate, MySQL, React, Bootstrap and axios.
+ 
---
+++ CrudFullstackWithReact/backend.spring/src/main/java/com/fullstack/backend/spring/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.fullstack.backend.spring.repository;
+
+import com.fullstack.backend.spring.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+}
